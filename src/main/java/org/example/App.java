1b059428086e0a8a6c4b00c3f9ac21f47cf62f45
jatin <jatinsrivastav.mrj@gmail.com>
2024-01-09 17:28:11 +0530

    BST sorted array

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -11,13 +11,25 @@ public class App
     public static void main( String[] args )
     {
 
-        System.out.println( "Hello World!" );
+        /*System.out.println( "Hello World!" );
         Scanner scanner =new Scanner(System.in);
         BinaryTree binaryTree =new BinaryTree();
         binaryTree.populate(scanner);
         //binaryTree.display();
 
-        binaryTree.prettyDisplay();
+        binaryTree.prettyDisplay();*/
+
+
+        BST bst =new BST();
+        /*int[] nums={5,8,9,2,7,4};
+        bst.populate(nums);
+        bst.display();*/
+
+        int[] nums={1,2,3,4,5,6,7,8,9,10};
+        bst.populateSortedArray(nums);
+        bst.display();
+
+
 
 
     }
---
+++ src/main/java/org/example/BST.java
@@ -0,0 +1,113 @@
+package org.example;
+
+public class BST {
+    public class Node {
+        private int value;
+        private Node left;
+        private Node right;
+        private int height;
+
+
+        public Node(int value) {
+            this.value = value;
+        }
+
+
+        public int getValue() {
+            return value;
+        }
+
+
+    }
+
+    private Node root;
+
+    public BST() {
+
+    }
+
+    public int height(Node node) {
+        if (node == null) {
+            return -1;
+        }
+        return node.height;
+    }
+
+
+    public boolean isEmpty() {
+        return root == null;
+    }
+
+
+    public void insert(int value) {
+        root = insert(value, root);
+    }
+
+    private Node insert(int value, Node node) {
+
+        if (node == null) {
+            node = new Node(value);
+            return node;
+        }
+
+        if (value < node.value) {
+            node.left = insert(value, node.left);
+        }
+
+        if (value > node.value) {
+            node.right = insert(value, node.right);
+        }
+        node.height = Math.max(height(node.left), height(node.right)) + 1;
+        return node;
+    }
+
+    public boolean balancedTree() {
+        return balancedTree(root);
+    }
+
+    private boolean balancedTree(Node node) {
+        if (node == null) {
+            return true;
+        }
+
+        return Math.abs(height(node.left) - height(node.right)) <= 1 && balancedTree(node.left) && balancedTree(node.right);
+    }
+
+
+    public void display() {
+        display(this.root, "Root node :");
+    }
+
+    private void display(Node node, String details) {
+        if (node == null) {
+            return;
+        }
+        System.out.println(details + node.value);
+        display(node.left, "left child of " + node.value + ":");
+        display(node.right, "right child of " + node.value + ":");
+    }
+
+    public void populate(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            this.insert(nums[i]);
+        }
+    }
+
+    public void populateSortedArray(int[] nums){
+        populateSortedArray(nums, 0, nums.length);
+
+    }
+
+    private void populateSortedArray(int[] nums, int start, int end) {
+      if (start>=end){
+          return;
+      }
+
+
+       int mid =(start+end)/2;
+       this.insert(nums[mid]);
+       populateSortedArray(nums, start, mid);
+       populateSortedArray(nums, mid+1, end);
+
+    }
+}
